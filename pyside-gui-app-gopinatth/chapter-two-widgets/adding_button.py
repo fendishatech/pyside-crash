591f038e7514017ddb6c7cc92b02570ebfc44dc9
fendishatech <liyukibir@gmail.com>
2023-05-30 05:48:43 -0700

    adding button

--- pyside-gui-app-gopinatth/chapter-two-widgets/adding_button.py
+++ pyside-gui-app-gopinatth/chapter-two-widgets/adding_button.py
@@ -1,26 +1,51 @@
 import sys
 from typing import Optional
 import PySide6.QtCore
-from PySide6.QtWidgets import QApplication,QWidget,QPushButton
+from PySide6.QtWidgets import QApplication,QWidget,QPushButton,QMessageBox
 
 
 class ButtonExample(QWidget):
-    def __init__(self) -> None:
+    def __init__(self, app) -> None:
         super().__init__()
 
+        self.app = app
+
         self.initGUI()
+        self.setButton()
 
     def initGUI(self):
         self.setWindowTitle("Button Example")
         self.resize(400, 325)
 
-    def setButton(self):...
+    def quitConfirmation(self, title, message):
+        userInfo = QMessageBox.question(self, title, message, QMessageBox.Yes | QMessageBox.No)
+        if userInfo == QMessageBox.Yes:
+            return True
+        if userInfo == QMessageBox.No:
+            return False
+
+    def quitApp(self):
+        response = self.quitConfirmation("Confirmation","This will quit the application. Do you want to continue?")
+
+        if response == True:
+            self.app.quit()
+        else: pass
+
+
+    def setButton(self):
+        """
+        A function to add a quit button.
+        """
+        quit_button = QPushButton("Quit",self)
+        quit_button.move(50,100)
+        quit_button.clicked.connect(self.quitApp)
+
 
 def main():
     try:
         app = QApplication()
 
-        button_eg = ButtonExample()
+        button_eg = ButtonExample(app)
         button_eg.show()
 
         sys.exit(app.exec())
